89d95ed524e10e4d9d2579c237e2191cddeb0c0a
Kristina <kristinamantha@icloud.com>
2020-04-17 16:07:04 -0400

    First version, see chat in Teams

---
+++ app/controllers/OrdersController.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import models.Order;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Orders.index;
+import views.html.Orders.show;
+
+import javax.inject.Inject;
+import javax.swing.text.AbstractDocument;
+import java.util.Set;
+
+/**
+ * created by Kristina Mantha, Christopher Lefebvre, Gregory Ramos
+ */
+public class OrdersController extends Controller {
+
+    @Inject
+    FormFactory formFactory;
+
+    //shows all orders
+    public Result index(){
+        Set<Order> orders = Order.allOrders();
+
+        return ok(index.render(orders));
+    }
+
+    //creates an order
+    public Result createOrder(){
+        Form<Order> orderForm = formFactory.form(Order.class);
+
+        return null;
+    }
+
+    //saves the order
+    public Result saveOrder(){
+        // TODO add information;
+
+        return null;
+    }
+
+    //to edit an order
+    public Result editOrder(Integer id){
+        // TODO add information;
+
+        return null;
+    }
+
+    //updates the order
+    public Result updateOrder(){
+        // TODO add information;
+
+        return null;
+    }
+
+    //deletes an order
+    public Result deleteOrder(Integer id){
+        // TODO add information;
+
+        return null;
+    }
+
+    //shows order details
+    public Result showOrder(Integer id){
+        Order order = Order.bookID(id);
+        if(order == null){
+            return notFound("Sorry, but the order ID does not exist.");
+        }
+        return null;
+      //  return ok(showOrder.render(order));
+    }
+}
---
+++ app/models/Order.java
@@ -0,0 +1,65 @@
+package models;
+
+import java.util.*;
+import java.util.HashSet;
+
+/**
+ * created by Kristina Mantha, Christopher Lefebvre, Gregory Ramos
+ */
+public class Order {
+    public Integer id;
+    public String senderLastName;
+    public String senderFirstName;
+    public Integer senderID;
+    public String recipientLastName;
+    public String recipientFirstName;
+    public Integer recipientID;
+    public Double transactionAmount;
+
+    public Order(Integer id, String senderLastName, String senderFirstName, Integer senderID,
+                 String recipientLastName, String recipientFirstName, Integer recipientID,
+                 Double transactionAmount){
+
+        this.id = id;
+        this.senderLastName = senderLastName;
+        this.senderFirstName = senderFirstName;
+        this.senderID = senderID;
+        this.recipientLastName = recipientLastName;
+        this.recipientFirstName = recipientFirstName;
+        this.recipientID = recipientID;
+        this.transactionAmount = transactionAmount;
+    }
+
+    private static Set<Order> orders;
+
+    static {
+        orders = new HashSet<>();
+        orders.add(new Order(1, "Hanks", "Tom", 23486, "Wilson","Rita",23487,275.00));
+        orders.add(new Order(2, "Mantha", "Kristina", 197903, "Verebes","Peter",198209,300.00));
+        orders.add(new Order(3, "Ramos", "Gregory", 34837, "Lefebvre","Christopher",846353,45.00));
+        orders.add(new Order(4, "Lefebvre", "Christoper", 846353, "Ramos","Gregory",34837,50.00));
+    }
+
+    public static Set<Order> allOrders(){
+        return orders;
+    }
+
+    public static Order bookID(Integer id){
+        for (Order order : orders){
+            if(id.equals(order.id)){
+                return order;
+            }
+        }
+        return null;
+    }
+
+    public static void addOrder(Order order){
+
+        orders.add(order);
+    }
+
+    public static boolean deleteOrder(Order order){
+
+        return orders.remove(order);
+    }
+}
